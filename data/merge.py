8bda74a3030571f44bdb116ef3ebdb4697a1177a
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2023-02-28 09:47:06 +0600

    data

---
+++ data/merge.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+df =[]
+
+# li = os.getcwd()
+# print(li)
+for files in os.listdir('data'): 
+    # print(files)
+    if files.find('description')>-1:
+        data = pd.read_csv(f'data/{files}')
+        df.append(data)
+        
+df = pd.concat(df)
+df.to_csv('final_data.csv',index=False)
